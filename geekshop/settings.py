d6d2e5e9486fd5de8bdc9614ae2bc90b8df91e12
sotnikovuu <sotnikovuu@yandex.ru>
2022-04-19 21:46:57 +0300

    Lesson 11. Code style fix

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "authnapp",
     "basketapp",
     "adminapp",
+    "social_django",
+    "ordersapp",
 ]
 
 
@@ -68,6 +70,9 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "mainapp.context_processors.basket",
+                "social_django.context_processors.backends",
+                "social_django.context_processors.login_redirect",
+                "django.template.context_processors.media",
             ],
         },
     },
