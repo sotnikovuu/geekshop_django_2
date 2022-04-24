1eabaef3745b68668a683ce0290799c612b54ede
sotnikovuu <81047012+sotnikovuu@users.noreply.github.com>
2022-04-24 05:00:54 +0300

    Lesson 12 (#13)
    * lesson_12. lesson_12 ready

--- ordersapp/forms.py
+++ ordersapp/forms.py
@@ -23,3 +23,16 @@ class OrderItemForm(forms.ModelForm):
     class Meta:
         model = OrderItem
         exclude = ()
+
+
+class OrderItemForm(forms.ModelForm):
+    price = forms.CharField(label="цена", required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(OrderItemForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+
+    class Meta:
+        model = OrderItem
+        exclude = ()
