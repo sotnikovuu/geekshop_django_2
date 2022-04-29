d9331efd68b04bc3dec93e67a2f70a1266160790
sotnikovuu <81047012+sotnikovuu@users.noreply.github.com>
2022-04-29 21:53:57 +0300

    Lesson 13 (#14)
    * Lesson_13 - Lesson_13 ready

--- ordersapp/forms.py
+++ ordersapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+from mainapp.models import Product
 from ordersapp.models import Order, OrderItem
 
 
@@ -15,21 +16,11 @@ class OrderForm(forms.ModelForm):
 
 
 class OrderItemForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(OrderItemForm, self).__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs["class"] = "form-control"
-
-    class Meta:
-        model = OrderItem
-        exclude = ()
-
-
-class OrderItemForm(forms.ModelForm):
     price = forms.CharField(label="цена", required=False)
 
     def __init__(self, *args, **kwargs):
         super(OrderItemForm, self).__init__(*args, **kwargs)
+        self.fields["product"].queryset = Product.get_items()
         for field_name, field in self.fields.items():
             field.widget.attrs["class"] = "form-control"
 
